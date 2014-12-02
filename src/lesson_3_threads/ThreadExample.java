b8d3d88e4988700720cc87de9b234bd8e61d2406
alibabaih <alibabaih@gmail.com>
2014-12-02 17:31:18 +0400

    how to make a thread #1 and #2

--- src/lesson_3_threads/ThreadExample.java
+++ src/lesson_3_threads/ThreadExample.java
@@ -1,7 +1,10 @@
 package lesson_3_threads;
 
+//how build threads
+
 public class ThreadExample {
     public static void main(String[] args) {
+        //how to #1
         Thread t = new Thread(){
             @Override
             public void run() {
@@ -9,5 +12,15 @@ public class ThreadExample {
             }
         };
         t.start();
+        //how to #2
+        t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("thread2");
+            }
+        });
+        t.start();// set up thread
+        System.out.println("hello");
     }
 }
+//6 minute http://hexlet.org/lesson/java_102_3/

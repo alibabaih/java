7e9ef24adf1624f1d2ca18039fd89f99497c60b5
alibabaih <alibabaih@gmail.com>
2014-12-02 11:05:02 +0400

    add style and change access level through style instance

---
+++ src/lesson_1_enum/Style.java
@@ -0,0 +1,10 @@
+package lesson_1_enum;
+
+public class Style {
+
+    public static Style DOLLAR_STYLE = new Style();
+    public static Style LINE_STYLE = new Style();
+
+    private Style(){}
+
+}
--- src/lesson_1_enum/TextPrinter.java
+++ src/lesson_1_enum/TextPrinter.java
@@ -2,19 +2,26 @@ package lesson_1_enum;
 
 public class TextPrinter {
 
-    public static void showText(String text, int style){
-        switch (style){
-            case 0:
-                System.out.println("***" + text + "***");
-                break;
-            case 1:
-                System.out.println("|||" + text + "|||");
-                break;
+    public static void showText(String text, Style style){
+//        switch (style){
+//            case 0:
+//                System.out.println("***" + text + "***");
+//                break;
+//            case 1:
+//                System.out.println("|||" + text + "|||");
+//                break;
+//            default:
+//                System.out.println(" " + text + " ");
+//        }
+        if (style == Style.DOLLAR_STYLE){
+            System.out.println("$$$" + text + "$$$");
+        } else if (style == Style.LINE_STYLE){
+            System.out.println("|||" + text + "|||");
         }
     }
 
     public static void main(String[] args){
-        showText("hello", 1);
+        showText("hello", Style.DOLLAR_STYLE);
     }
 
 }

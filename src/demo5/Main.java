fdb33648043e79264b6b1d05b03d9fda631c3a30
alibabaih <alibabaih@gmail.com>
2014-12-11 17:13:54 +0400

    add multithread demo5

--- src/demo5/Main.java
+++ src/demo5/Main.java
@@ -2,10 +2,83 @@ package demo5;
 /**
 Взаимодействие поков
  */
-public class Main {
+public class Main extends Thread {
 
-    public static void main(String[] args) {
+    private boolean suspended = false;
+    private boolean terminated = false;
+
+    public Main(){
+        suspended = false;
+        terminated = false;
+    }
+    public Main (boolean susp){
+        suspended = susp;
+        terminated = false;
+    }
+    public void terminate (){
+        terminated = true;
+    }
+    public void my_suspend (){
+        if (!suspended)
+        {
+            suspended = true;
+        }
+    }
+    public void my_resume (){
+        if (suspended)
+        {
+            suspended = false;
+            synchronized(this)
+            {
+                notify();
+            }
+            System.out.print("go....");
+        }
+    }
+    public boolean is_suspended()	{
+        return suspended;
+    }
+
+    @Override
+    public void run(){
+        int count = 0;
+        try
+        {
+            while (!terminated)
+            {
+                synchronized (this)
+                {
+                    while (suspended)	wait();
+                }
+                System.out.println ("Count = " + count++);
+                Thread.sleep(100);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Main thr = new Main();
+        thr.start();
+
+        for(int i=0; i<3; i++) {
+            Thread.sleep(1000);
+            if (thr.is_suspended())
+                thr.my_resume();
+            else
+                thr.my_suspend();
+        }
+        thr.my_resume();
+        thr.terminate();
+
+        thr.join();
 
     }
 
 }
+//3:06
+
